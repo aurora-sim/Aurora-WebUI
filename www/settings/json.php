89ca6265c5b53b7b5b6c8e977c5131b17b6b51f3
RevolutionSmythe <asdfisbetterthanjkl@gmail.com>
2011-01-09 22:00:40 -0600

    Add the correct json poster (the one with the timeout).

--- www/settings/json.php
+++ www/settings/json.php
@@ -4,21 +4,27 @@ include("settings/config.php");
 
 function do_post_request($found)
 {
-  $params = array('http' => array(
+    $params = array('http' => array(
               'method' => 'POST',
               'content' => implode(',', $found)
             ));
-  if ($optional_headers !== null) {
-    $params['http']['header'] = $optional_headers;
-  }
-  $ctx = stream_context_create($params);
-  $fp = @fopen(WIREDUX_SERVICE_URL, 'rb', false, $ctx);
-  if (!$fp) {
-    throw new Exception("Problem with " . WIREDUX_SERVICE_URL . ", $php_errormsg");
+    if ($optional_headers !== null) {
+        $params['http']['header'] = $optional_headers;
+    }
+    $ctx = stream_context_create($params);
+    $timeout = 3;
+    $old = ini_set('default_socket_timeout', $timeout);
+    $fp = @fopen(WIREDUX_SERVICE_URL, 'rb', false, $ctx);
+    ini_set('default_socket_timeout', $old);
+    stream_set_timeout($file, $timeout);
+    stream_set_blocking($file, 0);
+    if (!$fp) {
+    //throw new Exception("Problem with " . WIREDUX_SERVICE_URL . ", $php_errormsg");
+      return false;
   }
   $response = @stream_get_contents($fp);
   if ($response === false) {
-    throw new Exception("Problem reading data from " . WIREDUX_SERVICE_URL . ", $php_errormsg");
+    //throw new Exception("Problem reading data from " . WIREDUX_SERVICE_URL . ", $php_errormsg");
   }
   return $response;
 }
