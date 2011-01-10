4da6df76c2ca2737f8e721b91a1dd2cc52379d9c
RevolutionSmythe <asdfisbetterthanjkl@gmail.com>
2011-01-09 22:06:42 -0600

    Update the region generator to make .ini files and clean up the file.

--- www/app/management/region-generate.php
+++ www/app/management/region-generate.php
@@ -19,15 +19,15 @@ require_once("uuid.inc.php");
 		list($count) = $DbLink->next_record();
 		if ($count > 0)
 		{
-		$GENERATED = "There is already a region at the coordinates ".$_POST[region_location_x].", ".$_POST[region_location_y].". XML couldn't be made";
+		$GENERATED = "There is already a region at the coordinates ".$_POST[region_location_x].", ".$_POST[region_location_y].". ini couldn't be made";
 		}
 		else
 		{		
-		$GENERATED = '<Root><Config sim_UUID="'.$_POST[region_uuid].'" sim_name="'.$_POST[region_name].'" sim_location_x="'.$_POST[region_location_x].'" sim_location_y="'.$_POST[region_location_y].'" internal_ip_address="0.0.0.0" internal_ip_port="'.$_POST[region_ip_port].'" allow_alternate_ports="false" external_host_name="'.$_POST[region_ip_hostname].'" master_avatar_uuid="00000000-0000-0000-0000-000000000000" master_avatar_first="'.$_POST[region_master_first].'" master_avatar_last="'.$_POST[region_master_last].'" master_avatar_pass="'.$_POST[region_master_password].'" lastmap_uuid="" lastmap_refresh="" nonphysical_prim_max="0" physical_prim_max="0" clamp_prim_size="false" object_capacity="0"></Root>';
+		$GENERATED = '['.$_POST[region_name].']RegionUUID="'.$_POST[region_uuid].'" Location="'.$_POST[region_location_x].','.$_POST[region_location_y].'" InternalAddress="0.0.0.0" InternalPort="'.$_POST[region_ip_port].'" AllowAlternatePorts="false" ExternalHostName="'.$_POST[region_ip_hostname].'"';
 		}
 	}
 ?>
-<DIV style="height:100%">
+<div style="height:100%">
 <br />
 <table width="80%" border="0" align="center" cellpadding="3" cellspacing="0" bgcolor="#CCCC00">
   <tr>
@@ -49,7 +49,7 @@ if($GENERATED)
     <td><table width="100%" border="0" cellspacing="0" cellpadding="10">
 	  <tr>
         <td bgcolor="#FFFFFF">
-          <div align="center"><b>This is your region XML file:</b></div></td>
+          <div align="center"><b>This is your region ini file:</b></div></td>
       </tr>
 	  <tr>
         <td bgcolor="#FFFFFF">
@@ -65,27 +65,27 @@ if($GENERATED)
 <form name="form1" method="POST" action="index.php?page=region-generate">
 <TABLE width="80%" border=0 align=center cellpadding="0" cellspacing="0">
    <TR>
-      <TD width="221" height="40" align="center" valign="bottom" background="images/main/regions_middle.jpg"><b>Region UUID</b></a></TD>
+      <TD width="221" height="40" align="center" valign="bottom" background="images/main/regions_middle.jpg"><b>Region UUID</b></TD>
 	  <TD width="221" height="40" align="center" valign="bottom" background="images/main/regions_middle.jpg"><b><input type=text name=region_uuid size=80 readonly value=<?$str = UUID::generate(UUID::UUID_TIME, UUID::FMT_STRING, "osgrid");echo $str;?>></b></a></TD>
     </TR>
     <TR style='BACKGROUND-COLOR: #e8eff5'>
-      <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><b>Region - Name</b></a></TD>
+      <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><b>Region - Name</b></TD>
       <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><input type=text name=region_name size=80></TD>
 	</TR>
     <TR style='BACKGROUND-COLOR: #e8eff5'>
-      <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><b>Region - Location X</b></a></TD>
+      <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><b>Region - Location X</b></TD>
       <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><input type=text name=region_location_x size=80></TD>
 	</TR>
     <TR style='BACKGROUND-COLOR: #e8eff5'>
-      <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><b>Region - Location Y</b></a></TD>
+      <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><b>Region - Location Y</b></TD>
       <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><input type=text name=region_location_y size=80></TD>
 	</TR>
     <TR style='BACKGROUND-COLOR: #e8eff5'>
-      <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><b>Region - IP Port</b></a></TD>
+      <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><b>Region - IP Port</b></TD>
       <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><input type=text name=region_ip_port size=80></TD>
 	</TR>
     <TR style='BACKGROUND-COLOR: #e8eff5'>
-      <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><b>Region - External Hostname</b></a></TD>
+      <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><b>Region - External Hostname</b></TD>
       <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><input type=text name=region_ip_hostname size=80></TD>
 	</TR>
 	<?
@@ -93,18 +93,19 @@ if($GENERATED)
 	list($first,$last) = $DbLink->next_record();
 	?>
     <TR style='BACKGROUND-COLOR: #e8eff5'>
-      <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><b>Region Owner - First Name</b></a></TD>
+      <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><b>Region Owner - First Name</b></TD>
       <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><input type=text name=region_master_first readonly size=80 value=<?echo $first?>></TD>
 	</TR>
     <TR style='BACKGROUND-COLOR: #e8eff5'>
-      <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><b>Region Owner - Last Name</b></a></TD>
+      <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><b>Region Owner - Last Name</b></TD>
       <TD width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><input type=text name=region_master_last readonly size=80 value=<?echo $last?>></TD>
 	</TR>
 	<TR style='BACKGROUND-COLOR: #e8eff5'>
-      <TD colspan=2 width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><input type=submit name=makefile value="Make the file"></a></TD>
+      <TD colspan=2 width="221" height="40" align="center" valign="middle" bgcolor="#FFFFFF"><input type=submit name=makefile value="Make the file"></TD>
 	</TR>
 </TABLE>
-</FORM>
+</form>
+</div>
 <?
 }
 ?>
