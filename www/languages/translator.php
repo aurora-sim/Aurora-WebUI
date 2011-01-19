af02c94100c47e7e6c186b3fb157aa04aafc2c78
RevolutionSmythe <asdfisbetterthanjkl_AT_gmail.com>
2011-01-19 17:00:40 -0600

    Add translation files for German, Dutch, and Italian.

--- www/languages/translator.php
+++ www/languages/translator.php
@@ -2,9 +2,10 @@
 <?php
 if(!empty($_COOKIE['lang']))$lang=$_COOKIE['lang'];
 if(!empty($_GET['lang']))$lang=$_GET['lang'];
-if(!empty($lang) && ($lang=='eng' || $lang=='fr' || $lang=='esp')){
+if(!empty($lang) && ($lang=='eng' || $lang=='fr' || $lang=='esp'
+        || $lang=='nl' || $lang=='it' || $lang=='de')){
   include('lang_'.$lang.'.php');
   setcookie('lang',$lang,time()+3600*25*365,'/');
-}else include('languages/lang_eng.php');
+}else include('languages/lang_eng.php'); //Eng by default
 // echo "wiredux_welcome = $wiredux_welcome";
 ?>
--- www/languages/translator_page.php
+++ www/languages/translator_page.php
@@ -4,5 +4,8 @@
 <a href="?lang=fr"><img src="images/flags/flag-fr.png" alt="French" title="French" /></a>
 <a href="?lang=eng"><img src="images/flags/flag-eng.png" alt="English" title="English" /></a>
 <a href="?lang=esp"><img src="images/flags/flag-esp.png" alt="Spain" title="Spain" /></a>
+<a href="?lang=it"><img src="images/flags/flag-it.png" alt="Italian" title="Italian" /></a>
+<a href="?lang=du"><img src="images/flags/flag-nl.png" alt="Dutch" title="Dutch" /></a>
+<a href="?lang=du"><img src="images/flags/flag-de.png" alt="German" title="Dutch" /></a>
 <?php echo "$wiredux_actual_language"; ?>
 </div></div>
