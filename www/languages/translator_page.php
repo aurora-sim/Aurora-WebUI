073b648e3f5885819c5f78fadb4619e90d708cef
RevolutionSmythe <asdfisbetterthanjkl_AT_gmail.com>
2011-01-20 00:11:46 -0600

    Don't reset to the home page when changing languages.

--- www/languages/translator_page.php
+++ www/languages/translator_page.php
@@ -1,11 +1,11 @@
 <!-- Test Translation  -->
 <div id="Translator_Module">
     <div class="textcolorwhite">
+        <?php echo $wiredux_actual_language; ?>
         <?
         foreach ($languages as $langCode => $langName) {
             if ($langCode != $wiredux_language_code) {
-                echo ' <a href="?lang=' . $langCode . '"><img src="images/flags/flag-' . $langCode . '.png" alt=" ' . $langName . '" title=" ' . $langName . '" /></a>';
+                echo ' <a href="?page='.$_GET[page].'&btn='.$_GET[btn].'&lang=' . $langCode . '"><img src="images/flags/flag-' . $langCode . '.png" alt=" ' . $langName . '" title=" ' . $langName . '" /></a>';
             }
         } ?>
-        <?php echo $wiredux_actual_language; ?>
     </div></div>
