9caf8a68c4b1cac6ddc91c8ddd2b6e7fd2b3d537
RevolutionSmythe <asdfisbetterthanjkl_AT_gmail.com>
2011-01-20 17:29:47 -0600

    Add the dutch translation by Jeroen (phrearch), thanks!

--- www/languages/lang_nl.php
+++ www/languages/lang_nl.php
@@ -1,83 +1,83 @@
 <?php
-$wiredux_welcome='hi welcome';
+$wiredux_welcome='hallo welkom';
 $wiredux_language_code='nl';
 $wiredux_actual_language='Dutch';
 $wiredux_grid_status='GRID STATUS';
-$wiredux_total_users='Total Users';
-$wiredux_total_regions='Total Regions';
-$wiredux_unique_visitors='Unique Visitors last 30 days';
-$wiredux_online_now='Online Now';
-$wiredux_first_name='First Name';
-$wiredux_last_name='Last Name';
-$wiredux_password='Password';
-$wiredux_forgot_password='Forgotten Password?';
+$wiredux_total_users='Totaal Gebruikers';
+$wiredux_total_regions='Totaal Regio\'s';
+$wiredux_unique_visitors='Unieke Gebruikers in de laatste 30 dagen';
+$wiredux_online_now='Online Nu';
+$wiredux_first_name='Voornaam';
+$wiredux_last_name='Achternaam';
+$wiredux_password='Wachtwoord';
+$wiredux_forgot_password='Wachtwoord vergeten?';
 $wiredux_login='Login';
-$wiredux_confirm='Confirm';
-$wiredux_start_region='Start Region';
-$wiredux_address='Address';
-$wiredux_zip_code='Zip Code';
-$wiredux_city='City';
-$wiredux_country='Country';
-$wiredux_date_of_birth='Date Of Birth';
+$wiredux_confirm='Bevestig';
+$wiredux_start_region='Start Regio';
+$wiredux_address='Adres';
+$wiredux_zip_code='Postcode';
+$wiredux_city='Stad';
+$wiredux_country='Land';
+$wiredux_date_of_birth='Geboortedatum';
 $wiredux_email='Email';
-$wiredux_create_new_account='Create New Account';
-$wiredux_world_map='World Map';
-$wiredux_region_name='Region Name';
-$wiredux_location='Location';
+$wiredux_create_new_account='Registreer nieuw account';
+$wiredux_world_map='Wereldmap';
+$wiredux_region_name='Regio naam';
+$wiredux_location='Locatie';
 $wiredux_info='Info';
-$wiredux_regions_found='Regions Found';
-$wiredux_region_list='Region List';
-$wiredux_region_list_page_info='This is the regions list, click on a region\'s name to get more information about that region.';
-$wiredux_more_info='More Info';
-$wiredux_user_name='User Name';
-$wiredux_online_users_info='This shows all the users that are online at this moment.';
-$wiredux_submit='Submit';
-$wiredux_verified_account='Thank you, your account is now active and ready to use.';
-$wiredux_internal_error='Internal error, please try again later.';
-$wiredux_invalid_code='This isnt a valid code or maybe the code was older than 24h';
-$wiredux_activate_account='Activate Account';
-$wiredux_change_account='Change Account';
-$wiredux_change_home_region='Change Home Region';
-$wiredux_old_region='Old Region';
-$wiredux_change_password='Change Password';
-$wiredux_old_password='Old Password';
-$wiredux_change_email='Change Email';
-$wiredux_old_email='Old Email';
-$wiredux_change_name='Change Name';
+$wiredux_regions_found='Regio\'s Gevonden';
+$wiredux_region_list='Regio Overzicht';
+$wiredux_region_list_page_info='Dit is het regio overzicht. Klik op de regio\'s naam om meer informatie over deze regio te verkrijgen.';
+$wiredux_more_info='Meer Informatie';
+$wiredux_user_name='Gebruikersnaam';
+$wiredux_online_users_info='Dit toont alle gebruikers die online zijn op dit moment.';
+$wiredux_submit='Verzend';
+$wiredux_verified_account='Dank u, uw profiel is nu actief en gereed voor gebruik.';
+$wiredux_internal_error='Interne fout, probeert u het later nog eens.';
+$wiredux_invalid_code='Dit is geen geldige code(is de code ouder dan 24 uur?)';
+$wiredux_activate_account='Activeer Profiel';
+$wiredux_change_account='Wijzig Profiel';
+$wiredux_change_home_region='Wijzig Thuis Regio';
+$wiredux_old_region='Oude Regio';
+$wiredux_change_password='Wijzig Wachtwoord';
+$wiredux_old_password='Oud Wachtwoord';
+$wiredux_change_email='Wijzig Email';
+$wiredux_old_email='Oude Email';
+$wiredux_change_name='Wijzig Naam';
 $wiredux_login='Login';
-$wiredux_register='Register';
-$wiredux_news='News';
-$wiredux_history='History';
+$wiredux_register='Registreer';
+$wiredux_news='Nieuws';
+$wiredux_history='Historie';
 
 //
 //Menu items
 //
-$wiredux_menu_item_1_name = 'Home';
-$wiredux_menu_item_2_name = 'Edit Account';
-$wiredux_menu_item_3_name = 'News';
-$wiredux_menu_item_4_name = 'Transaction History';
-$wiredux_menu_item_5_name = 'Region List';
-$wiredux_menu_item_6_name = 'Map';
-$wiredux_menu_item_7_name = 'Register';
-$wiredux_menu_item_8_name = 'Logout';
+$wiredux_menu_item_1_name = 'Thuis';
+$wiredux_menu_item_2_name = 'Wijzig Profiel';
+$wiredux_menu_item_3_name = 'Nieuws';
+$wiredux_menu_item_4_name = 'Transactie Historie';
+$wiredux_menu_item_5_name = 'Regio Overzicht';
+$wiredux_menu_item_6_name = 'Kaart';
+$wiredux_menu_item_7_name = 'Registreer';
+$wiredux_menu_item_8_name = 'Loguit';
 $wiredux_menu_item_9_name = 'Login';
-$wiredux_menu_item_10_name = 'Who\'s Online';
+$wiredux_menu_item_10_name = 'Wie is Online';
 
 //
 // Home Page
 //
 
-$wiredux_home_page='<h2><strong>Welcome to the new Aurora WiRedux webpage!</strong></h2>
-    <p>
-        This is our New Virtual World where you can have fun, meet people, chat, play, explore, create and share your dreams. <br />
-        Everything is possible in our brand new 3D World. <br />
-        Our World is created by its residents, you can build everything you can imagine here. <br /><br />
-        Create a free account today, build a virtual you and play in our world. <br />
-        Join us now, and make a difference! <br /><br /><br />
-        <u>There are just 3 steps to get started:</u> <br /><br />
-        1.) Create your FREE Account right here on our pages. <br /><br />
-        2.) Download the viewer. We recommend <a href="http://wiki.kokuaviewer.org/wiki/Downloads">Imprudence / Kokua </a> <br /><br />
-        3.) Be sure to add our grid to your viewer. <br /><br /><br />
-        <i>Please keep in mind that the Aurora platform is still in alpha release and so may not always perform as expected.</i>
-    </p>';
+$wiredux_home_page='<h2><strong>Welkom bij de Aurora WiRedux webpagina!</strong></h2>
+<p>
+Dit is onze nieuwe virtuele wereld, waar je plezier kan hebben, mensen kan ontmoeten, chatten, spelen, onderzoeken en dromen kan delen en bouwen.<br />
+Alles is mogelijk in onze gloednieuwe 3d wereld.<br />
+Onze wereld wordt gemaakt door haar inwoners, en alles wat je maar kan voorstellen kan gebouwd worden.<br /><br />
+Maak vandaag nog een gratis account aan, ontwerp je avatar en ontdek onze wereld.<br />
+Volg ons voorbeeld, en maak het verschil!<br /><br /><br />
+<u>Er zijn slechts 3 stappen nodig om te beginnen:</u> <br /><br />
+1.) Maak een GRATIS account aan, hier op de webpagina. <br /><br />
+2.) Download de viewer. We raden aan om <a href="http://wiki.kokuaviewer.org/wiki/Downloads">Imprudence / Kokua </a> te gebruiken.<br /><br />
+3.) Voeg ons grid toe aan de viewer. <br /><br /><br />
+<i>Hou wel in gedachten dat het Aurora platform nog steeds experimenteel is, en dat sommige dingen (nog) niet zoals verwacht kunnen werken.</i>
+</p>';
 ?>
