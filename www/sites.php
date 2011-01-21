ab6afee243631ff554930666b5b6a8057304d5fe
RevolutionSmythe <asdfisbetterthanjkl_AT_gmail.com>
2011-01-20 18:11:57 -0600

    Clean up sites.php.

--- www/sites.php
+++ www/sites.php
@@ -76,12 +76,6 @@ switch ($_SESSION[page]) {
 
         return;
 
-    case 'transactions':
-
-        include("./sites/transactions.php");
-
-        return;
-
     case 'regions':
 
         include("./sites/regionlist.php");
@@ -91,32 +85,6 @@ switch ($_SESSION[page]) {
     case 'map':
 
         include("./sites/map.php");
-
-        return;
-
-    // Additions for Search
-
-    case 'events':
-
-        include("./sites/events.php");
-
-        return;
-
-    case 'make-events':
-
-        include("./sites/make-events.php");
-
-        return;
-
-    case 'save-events':
-
-        include("./sites/save-events.php");
-
-        return;
-
-    case 'classifieds':
-
-        include("./sites/classifieds.php");
 
         return;
 
@@ -133,62 +101,6 @@ switch ($_SESSION[page]) {
     case 'online':
 
         include("./sites/whosonline.php");
-
-        return;
-
-    // Added Accounting Tools
-
-    case 'accounting':
-
-        include("./sites/accounting.php");
-
-        return;
-
-    // Adding Statistics Information
-
-    case 'stats':
-
-        include("./sites/stats.php");
-
-        return;
-
-    // Adding Region Tools
-
-    case 'regiontools':
-
-        include("./app/management/regiontools.php");
-
-        return;
-
-    case 'region-generate':
-
-        include("./app/management/region-generate.php");
-
-        return;
-
-    case 'region-overview':
-
-        include("./app/management/region-overview.php");
-
-        return;
-
-    case 'region-services':
-
-        include("./app/management/region-services.php");
-
-        return;
-
-    // Adding Support
-
-    case 'support':
-
-        include("./sites/support.php");
-
-        return;
-
-    case 'login':
-
-        include("./sites/login.php");
 
         return;
 
