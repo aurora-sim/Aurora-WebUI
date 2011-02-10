3568d6fb70ae41451e0ffddbb4cef5eb79a89eb1
RevolutionSmythe <asdfisbetterthanjkl@gmail.com>
2011-02-10 07:54:11 -0800

    Update the admin settings table with the new verify users and allow registration parts.

--- www/sql_update/wi_adminsetting.sql
+++ www/sql_update/wi_adminsetting.sql
@@ -7,8 +7,10 @@ CREATE TABLE `wi_adminsetting` (
   `lastnames` varchar(10) NOT NULL,
   `adress` varchar(32) NOT NULL,
   `region` text NOT NULL,
+  `allowRegistrations` varchar(10) NOT NULL,
+  `verifyUsers` varchar(10) NOT NULL,
   PRIMARY KEY  (`id`)
 ) TYPE=MyISAM  AUTO_INCREMENT=2 ;
 
-INSERT INTO `wi_adminsetting` (`id`, `startregion`, `userdir`, `griddir`, `assetdir`, `lastnames`, `adress`, `region`) VALUES 
-(1, '', '', '', '', '0', '0', '0');
+INSERT INTO `wi_adminsetting` (`id`, `startregion`, `userdir`, `griddir`, `assetdir`, `lastnames`, `adress`, `region`,'allowRegistrations','verifyUsers') VALUES 
+(1, '', '', '', '', '0', '0', '0','1','1');
