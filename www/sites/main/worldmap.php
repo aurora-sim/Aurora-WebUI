320ff15d91a025b3b8d97a764b263e42b5d1c5c5
RevolutionSmythe <asdfisbetterthanjkl_AT_gmail.com>
2011-02-15 17:57:02 -0600

    Remove a bad .html file that was borking the map. Fix the full screen part of the worldmap so that it builds the right URL.

--- www/sites/main/worldmap.php
+++ www/sites/main/worldmap.php
@@ -1,5 +1,5 @@
 <div id="content">
-  <h2><?= SYSNAME ?>: <? echo $wiredux_world_map ?> <a <?= "onclick=\"window.open('".SYSURL."/app/map/','mywindow','width=1000,height=1000')\"" ?>>Full Screen</a></h2>
+  <h2><?= SYSNAME ?>: <? echo $wiredux_world_map ?> <a <?= "onclick=\"window.open('".SYSURL."app/map/index.php','mywindow','width=1000,height=1000')\"" ?>>Full Screen</a></h2>
     <div id="region_map">
       <iframe src="<?=SYSURL?>/app/map/index.php" frameborder="0" width="100%" height="100%">
         <p>Your browser does not support iframes.</p>
