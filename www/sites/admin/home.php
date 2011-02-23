04cdfc97ccbbc5f245b950b80bf745706c4ab990
skidzTweak <skidz.tweak_@_gmail.com>
2011-02-23 02:56:58 -0600

    Added links to sub menus items on Admin home page. Fixed a mistake in one of the SQL files

--- www/sites/admin/home.php
+++ www/sites/admin/home.php
@@ -12,6 +12,31 @@ if ($_SESSION[ADMINID]) {
   <div id="info">
       <p><? echo $webui_admin_home_info; ?></p>
   </div>
+  
+  <div>
+	<p>
+	<?
+	$DbLink2 = new DB;
+	$DbLink = new DB;
+	if ($_SESSION[USERID])
+		$Display = 1;
+	else
+		$Display = 0;
+
+	if($_SESSION[ADMINID])
+		$AdminDisplay = " or (display='3')";
+	else
+		$AdminDisplay = "";
+	$DbLink2->query("SELECT id,url,target FROM " . C_PAGE_TBL . " Where parent = '$_GET[btn]' and active='1' and ((display='$Display') or (display='2') " . $AdminDisplay . ") ORDER BY rank ASC ");
+	$a = get_defined_vars();
+	while (list($siteid, $siteurl, $sitetarget) = $DbLink2->next_record()) 
+	{
+		echo "<a href=\"$siteurl&btn=$siteid\"><span>$a[$siteid]</span></a><br/>";
+	}
+		
+	?>
+	</p>
+  </div>
 
   <? } else { ?>
   
