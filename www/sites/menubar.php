07caa3e94abd1f43e800b782d1888a3b42960afe
skidzTweak <skidz.tweak_@_gmail.com>
2011-02-23 15:18:22 -0600

    Finishing up drop down menus

--- www/sites/menubar.php
+++ www/sites/menubar.php
@@ -18,7 +18,7 @@
 				echo "<li><a href=\"#\"><span>$a[$siteid]</span></a>";
 				if (count($DbLink2) > 0)
 				{
-					echo "<ul class='menu' style='visibility:hidden;'>";
+					echo "<ul>";
 					while (list($siteid2, $siteurl2, $sitetarget2) = $DbLink2->next_record()) 
 					{
 						WriteMenu($siteid2, $siteurl2, $sitetarget2, $a, $Display);
@@ -32,7 +32,7 @@
 				echo "<li><a href=\"$siteurl&btn=$siteid\"><span>$a[$siteid]</span></a>";
 				if (count($DbLink2) > 0)
 				{
-					echo "<ul class='menu' style='visibility:hidden;'>";
+					echo "<ul>";
 					while (list($siteid2, $siteurl2, $sitetarget2) = $DbLink2->next_record()) 
 					{
 						WriteMenu($siteid2, $siteurl2, $sitetarget2, $a, $Display);
@@ -47,7 +47,7 @@
 			echo "<li><a href=\"#\" onclick=\"window.open('$siteurl','mywindow','width=400,height=200')\"><span>$a[$siteid]</span></a>";
 			if (count($DbLink2) > 0)
 			{
-				echo "<ul class='menu' style='visibility:hidden;'>";
+				echo "<ul>";
 				while (list($siteid2, $siteurl2, $sitetarget2) = $DbLink2->next_record()) 
 				{
 					WriteMenu($siteid2, $siteurl2, $sitetarget2, $a, $Display);
@@ -65,27 +65,27 @@
  
 ?>
 <div id="menu">
-    <ul class="menu">
+    <ul>
     
     <?
-    $DbLink = new DB;
-    if ($_SESSION[USERID])
-        $Display = 1;
-    else
-        $Display = 0;
-    
-    if($_SESSION[ADMINID])
-        $AdminDisplay = " or (display='3')";
-    else
-        $AdminDisplay = "";
+		$DbLink = new DB;
+		if ($_SESSION[USERID])
+			$Display = 1;
+		else
+			$Display = 0;
+		
+		if($_SESSION[ADMINID])
+			$AdminDisplay = " or (display='3')";
+		else
+			$AdminDisplay = "";
 
-    $DbLink->query("SELECT id,url,target FROM " . C_PAGE_TBL . " Where parent is null and active='1' and ((display='$Display') or (display='2') " . $AdminDisplay . ") ORDER BY rank ASC ");
-    // $tableWidth = 1000 / $DbLink->num_rows();
-    $a = get_defined_vars();
-	while (list($siteid, $siteurl, $sitetarget) = $DbLink->next_record()) 
-	{
-		WriteMenu($siteid, $siteurl, $sitetarget, $a, $Display);
-	}
+		$DbLink->query("SELECT id,url,target FROM " . C_PAGE_TBL . " Where parent is null and active='1' and ((display='$Display') or (display='2') " . $AdminDisplay . ") ORDER BY rank ASC ");
+		// $tableWidth = 1000 / $DbLink->num_rows();
+		$a = get_defined_vars();
+		while (list($siteid, $siteurl, $sitetarget) = $DbLink->next_record()) 
+		{
+			WriteMenu($siteid, $siteurl, $sitetarget, $a, $Display);
+		}
     ?>
     </ul>
 </div>
