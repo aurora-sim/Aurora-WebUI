48f1c48489c61eef29460f4aa3ecc7429c100d2a
skidzTweak <skidz.tweak_@_gmail.com>
2011-02-27 10:26:42 -0600

    Fixes for gallery menu. Thanks DJ

--- www/sql_update/wi_pagemanager.sql
+++ www/sql_update/wi_pagemanager.sql
@@ -26,7 +26,7 @@ INSERT INTO `wi_pagemanager` (`id`, `rank`, `active`, `url`, `target`, `display`
 ('webui_menu_item_news', 4.1, '1', 'index.php?page=news', '_self', '2', 'webui_menu_item_world'),
 ('webui_menu_item_regions', 4.2, '1', 'index.php?page=regionlist', '_self', '2','webui_menu_item_world'),
 ('webui_menu_item_worldmap', 4.3, '1', 'index.php?page=worldmap', '_self', '2', 'webui_menu_item_world'),
-('webui_menu_item_gallery', 4.3, '1', 'index.php?page=gallery', '_self', '2', 'webui_menu_item_gallery'),
+('webui_menu_item_gallery', 4.4, '1', 'index.php?page=gallery', '_self', '2', 'webui_menu_item_world'),
 
 ('webui_menu_item_users', 5.0, '1', 'index.php?page=users', '_self', '1', null),
 ('webui_menu_item_peoplesearch', 5.1, '1', 'index.php?page=peoplesearch', '_self', '1', 'webui_menu_item_users'),
