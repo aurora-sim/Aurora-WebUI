958d0a61d3130913182b0ac123fe8c6e9e6cd143
skidzTweak <skidz.tweak_@_gmail.com>
2011-03-03 21:49:35 -0600

    Fixed admin login

--- www/sites/admin/home.php
+++ www/sites/admin/home.php
@@ -3,19 +3,15 @@ if ($_SESSION[ADMINID]) {
 ?>
 
 <div id="content">
-  <h2><?= SYSNAME ?>: <? echo $webui_admin_home ?></h2>
-
-  <? echo $webui_welcome_userid; ?>
-  
-  <h1><? echo $webui_admin_welcome; ?> <? echo $webui_admin_panel; ?> <?= SYSNAME ?></h1>
-
-  <div id="info">
-      <p><? echo $webui_admin_home_info; ?></p>
-  </div>
-  
-  <div>
+	<h2><?= SYSNAME ?>: <? echo $webui_admin_home ?></h2>
+	<? echo $webui_welcome_userid; ?>
+	<h1><? echo $webui_admin_welcome; ?> <? echo $webui_admin_panel; ?> <?= SYSNAME ?></h1>
+	<div id="info">
+		<p><? echo $webui_admin_home_info; ?></p>
+	</div>
+	<div>
 	<p>
-	<?
+<?
 	$DbLink2 = new DB;
 	$DbLink = new DB;
 	if ($_SESSION[USERID])
@@ -33,29 +29,33 @@ if ($_SESSION[ADMINID]) {
 	{
 		echo "<a href=\"$siteurl&btn=$siteid\"><span>$a[$siteid]</span></a><br/>";
 	}
-		
-	?>
+?>
 	</p>
-  </div></div>
+	</div>
+</div>
 
   <? } else { ?>
-<div id="content">  <h2><?= SYSNAME ?>: <? echo $webui_admin_login ?></h2>      <div id="login">        <form action="index.php" method="POST" onSubmit="return Form(this)">                <table>          <tr>            <td class="even"> <? echo $webui_first_name ?></td>            <td class="even"><input id="login_input" name="logfirstname" type="text" class="box" value="<?= $_POST[logfirstname] ?>" /> </td>          </tr>  
-          <tr>
-            <td class="odd"> <? echo $webui_last_name ?></td>
-            <td class="odd"><input id="login_input" name="loglastname" type="text" class="box" value="<?= $_POST[loglastname] ?>" /></td>
-          </tr>
-
-          <tr>
-            <td class="even"> <? echo $webui_password ?></td>
-            <td class="even"><input id="login_input" type="password" name="logpassword" class="box"/></td>
-          </tr>
-
-          <tr>
-            <td class="odd"></td>
-            <td class="odd"><input id="login_bouton" style="cursor:pointer" type="submit" name="Submit" value="<? echo $webui_admin_login ?>" /></td>
-          </tr>
-        </table>
-        </form>
-      </div>
+	<div id="content">  
+		<h2><?= SYSNAME ?>: <? echo $webui_admin_login ?></h2>      
+		<div id="login">        
+			<form action="index.php" method="POST" onsubmit="if (!validate(this)) return false;">
+				<table>
+					<tr><td class="error" colspan="2" align="center" id="error_message"><?=$_SESSION[ERROR];$_SESSION[ERROR]="";?><?=$_GET[ERROR]?></td></tr>
+					<tr>
+						<td class="even"><span id="logname_label"><? echo $webui_user_name ?>*</span></td>
+						<td class="even"><input require="true" label="logname_label" id="login_input" name="logname" type="text" value="<?= $_POST[logname] ?>" /></td>
+					</tr>
+					<tr>
+						<td class="even"><span id="password_label"><? echo $webui_password ?>*</span></td>
+						<td class="even"><input require="true" label="password_label" id="login_input" type="password" name="logpassword" /></td>
+					</tr>
+					<tr>
+						<td class="odd"><a href="index.php?page=forgotpass"><? echo $webui_forgot_password ?></a></td>
+						<td class="odd"><input id="login_bouton" type="submit" name="Submit" value="<? echo $webui_admin_login ?>" /></td>
+					</tr>
+				</table>
+			</form>
+		</div>
+	</div>
   <? } ?>
-</div>
+
