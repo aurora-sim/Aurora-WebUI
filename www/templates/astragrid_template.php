d6a3b9ac9f4e2bb0c57a63326ee7bfdab7405cf2
Patrick Sapinski <patrick@sapinski.com>
2011-03-22 14:29:53 -0400

    astragrid template

---
+++ www/templates/astragrid_template.php
@@ -0,0 +1,6 @@
+<?php
+$template_css = 'templates/astragrid/default_template.css';
+$notfound_image = 'templates/astragrid/error_404.png';
+$favicon_image = 'templates/astragrid/favicon.ico';
+$images_path = 'templates/astragrid/';
+?>
