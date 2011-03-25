e07c4cf32afb6340dbff4b6443f676ba4bac67e7
MattoDestiny <matto.destiny@gmail.com>
2011-03-25 18:03:49 +0000

    merged all 14 sqls into 1 sql wi_webui.sql

--- www/sql_update/wi_appearance.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE `wi_appearance` (
-  `Enabled` varchar(36) NOT NULL default '',
-  `Picture` varchar(32) NOT NULL,
-  `ArchiveName` varchar(32) NOT NULL,
-  PRIMARY KEY  (`ArchiveName`)
-) TYPE=MyISAM;
--- www/sql_update/wi_codetable.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `wi_codetable` (
-  `UUID` varchar(36) NOT NULL,
-  `code` varchar(255) NOT NULL,
-  `info` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `time` varchar(255) NOT NULL
-) TYPE=MyISAM;
