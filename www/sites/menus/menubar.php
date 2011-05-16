8c4a1de5261d365746ed18023a514849a8c86379
RevolutionSmythe <asdfisbetterthanjkl_AT_gmail.com>
2011-05-15 20:43:32 -0500

    Add ability to add external links to the menubar by using the "_external" tag in the database.

--- www/sites/menus/menubar.php
+++ www/sites/menus/menubar.php
@@ -41,7 +41,20 @@
 				}
 				echo "</li>";
 			}
-		} 
+		}
+                else if ($sitetarget == '_external') {
+                    echo "<li><a href=\"$siteurl\"><span>$a[$siteid]</span></a>";
+                            if ($DbLink2->num_rows() > 0)
+                            {
+                                    echo "<ul>";
+                                    while (list($siteid2, $siteurl2, $sitetarget2) = $DbLink2->next_record())
+                                    {
+                                            WriteMenu($siteid2, $siteurl2, $sitetarget2, $a, $Display, $AdminDisplay);
+                                    }
+                                    echo "</ul>";
+                            }
+                            echo "</li>";
+                }
 		else 
 		{
 			echo "<li><a href=\"#\" onclick=\"window.open('$siteurl','mywindow','width=400,height=200')\"><span>$a[$siteid]</span></a>";
