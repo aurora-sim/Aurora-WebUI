4ec3a16d5445a72e781ca0d098791b936a5d8e59
RevolutionSmythe <asdfisbetterthanjkl_AT_gmail.com>
2011-06-02 15:25:37 -0500

    Update the news page so that it only shows 5 news entries at a time and has forward and back buttons to go through the history.

--- www/sites/news/news.php
+++ www/sites/news/news.php
@@ -1,8 +1,34 @@
 <div id="content">
+    <?php
+
+                $querypage = 0;
+                if($_GET[pagenum] != "") {
+                    $querypage = cleanQuery($_GET[pagenum]);
+                }
+                $showNext = true;
+                $DbLink = new DB;
+                $DbLink->query("SELECT COUNT(*) from " . C_NEWS_TBL . $query);
+while (list($count) = $DbLink->next_record()) {
+if($querypage*5 + 5 > $count)
+    $showNext = false;
+}
+                ?>
     <div id="annonce10">
     <center><h2> <? echo $webui_news; ?><p> </h2></center>
     <div id="info">
-            <p><strong><? echo $webui_news; ?></strong></p>
+        <div style="text-align: left; width: 50%; float: left;">
+        <?php
+        if($querypage > 0) { ?>
+            <a href="<?=SYSURL?>index.php?page=news&pagenum=<?=$querypage-1?>">Previous Page</a>
+            <?php } ?>&nbsp;
+        </div>
+        <div style="text-align: right; width: 50%; float: left;">
+            <?php
+            if($showNext) { ?>
+            <a href="<?=SYSURL?>index.php?page=news&pagenum=<?=$querypage+1?>">Next Page</a>
+            <?php } ?>&nbsp;
+        </div>
+        <br><center><p><strong><? echo $webui_news; ?>     </strong></p></center>
         </div>
             <table>
                 <?
@@ -10,15 +36,15 @@
                 if($_GET[scr] != "") {
                     $query = " where id='".cleanQuery($_GET[scr])."'";
                 }
-                $DbLink = new DB;
-                $DbLink->query("SELECT id,title,message,time from " . C_NEWS_TBL . $query . " ORDER BY time DESC");
+                $querypage = $querypage * 5;
+                $DbLink->query("SELECT id,title,message,time from " . C_NEWS_TBL . $query . " ORDER BY time DESC LIMIT $querypage,".($querypage+5));
                 $count = 0;
 
                 while (list($id, $title, $message, $time) = $DbLink->next_record()) {
                     $count++;
 
                     if (strlen($title) > 92) {
-                        $title = substr($title, 0, 64);
+                        $title = substr($title, 0, 92);
                         $title .= "...";
                     }
                     $TIMES = date("l M d Y", $time);
