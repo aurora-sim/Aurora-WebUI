c5308940009fbd6021aaa1083c86b5cff7dcdcdd
MattoDestiny <matto.destiny@gmail.com>
2011-08-18 14:27:13 +0100

    changed the options iwc, hg, voice and currency to false by default in config.php

--- www/settings/config.php
+++ www/settings/config.php
@@ -39,15 +39,15 @@ define("AddGrid_KokuaURL","http://your_webui_server_ip_or_dns:8002/");
 define("AddGrid_SecondLifeURL","http://your_webui_server_ip_or_dns:8002/");
 define("AddGrid_KristenURL","http://your_webui_server_ip_or_dns:8002/");
 
-$AddGrid_IWC_Actived = true;
+$AddGrid_IWC_Actived = false;
 define("AddGrid_IWC_URL_1","secondlife://your_webui_server_ip_or_dns:8003/");
 
-$AddGrid_HG_Actived = true;
+$AddGrid_HG_Actived = false;
 define("AddGrid_HG_URL_1","secondlife://your_webui_server_ip_or_dns:8003/");
 // define("HG_URL_2","http://slurll.com/secondlife/your_webui_server_ip_or_dns:8003:digigrids");
 
-$AddGrid_Voice_Actived = true;
-$AddGrid_Currency_Actived = true;
+$AddGrid_Voice_Actived = false;
+$AddGrid_Currency_Actived = false;
 
 ################### Logo Light Effect ###################
 // Should we display the logo 
