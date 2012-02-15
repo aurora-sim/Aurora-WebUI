7cf9e00aa75a427b142b09c52c2276c5ec848524
Enrico Nirvana <enrico.ranucci@live.it>
2012-02-15 21:39:52 +0100

    Revert a merge in the wrong branch

--- www/sites/admin/edit.php
+++ www/sites/admin/edit.php
@@ -1,11 +1,11 @@
-<?php
+<?
 if($_SESSION[ADMINID]){
 
 $DbLink = new DB;
 if($_POST[userdata]=="set")
 {
 $found = array();
-$found[0] = json_encode(array('Method' => 'EditUser', 'WebPassword' => md5(WEBUI_PASSWORD),
+$found[0] = json_encode(array('Method' => 'EditUser', 'WebPassword' => md5(WIREDUX_PASSWORD),
                     'UserID' => cleanQuery($_POST[userid]),
 					'Name' => cleanQuery($_POST[name]),
                     'RLName' => cleanQuery($_POST[rlname]),
@@ -39,7 +39,7 @@ window.location.href='index.php?page=adminedit&userid=".$_POST[userid]."';
 
 
 $found = array();
-$found[0] = json_encode(array('Method' => 'GetProfile', 'WebPassword' => md5(WEBUI_PASSWORD)
+$found[0] = json_encode(array('Method' => 'GetProfile', 'WebPassword' => md5(WIREDUX_PASSWORD)
             , 'UUID' => cleanQuery($_GET['userid'])));
 $do_post_requested = do_post_request($found);
 $recieved = json_decode($do_post_requested);
@@ -75,78 +75,77 @@ else
 
 
 <div id="content">
-  <div id="ContentHeaderLeft"><h5><?php echo SYSNAME ?></h5></div>
+  <div id="ContentHeaderLeft"><h5><?= SYSNAME ?></h5></div>
   <div id="ContentHeaderCenter"></div>
-  <div id="ContentHeaderRight"><h5><?php echo $webui_admin_edit_manage; ?></h5></div>   
+  <div id="ContentHeaderRight"><h5><? echo $webui_admin_edit_manage; ?></h5></div>   
   <div id="managepanel">
 
         <div id="info">
-            <p><?php echo $webui_admin_edit_manage_info; ?></p>
+            <p><? echo $webui_admin_edit_manage_info; ?></p>
         </div>
         
-		<div id="annonce10">
         <table>
             <form name="form1" method="post" action="index.php?page=adminedit">
             <input type="hidden" name="userdata" value="set" />
-            <input type="hidden" name="userid" value="<?php echo $uuid ?>" />
+            <input type="hidden" name="userid" value="<?=$uuid?>" />
                
             <tr>
-                <td class="odd" width="50%"><?php echo $webui_admin_edit_manage_userid; ?></td>
-                <td class="odd"><?php echo $uuid ?></td>
+                <td class="odd" width="50%"><? echo $webui_admin_edit_manage_userid; ?></td>
+                <td class="odd"><?=$uuid?></td>
             </tr>
             
       		  <tr>
-		            <td class="even"><?php echo $webui_admin_edit_manage_avatar_name; ?></td> 
-		            <td class="even"><input style="width:99%" name="name" type="text" id="name" value="<?php echo $name?>"</td>
+		            <td class="even"><? echo $webui_admin_edit_manage_avatar_name; ?></td> 
+		            <td class="even"><input style="width:99%" name="name" type="text" id="name" value="<?=$name?>"</td>
 		        </tr>
             
             <tr>
                 <td class="even"><? echo $webui_admin_edit_manage_real_name; ?></td>
                 <td class="even">
-                    <input style="width:99%" name="rlname" type="text" id="fname" value="<?php echo $rlname?>" />
+                    <input style="width:99%" name="rlname" type="text" id="fname" value="<?=$rlname?>" />
                 </td>
             </tr>
             
           
             <tr>
-                <td class="even"><?php echo $webui_admin_edit_manage_real_street; ?></td>
-                <td class="even"><input style="width:99%" name="street" type="text" id="street" value="<?php echo $street ?>" /></td>
+                <td class="even"><? echo $webui_admin_edit_manage_real_street; ?></td>
+                <td class="even"><input style="width:99%" name="street" type="text" id="street" value="<?=$street?>" /></td>
             </tr>
             
             <tr>
-                <td class="odd"><?php echo $webui_admin_edit_manage_real_zip; ?></td>
-                <td class="odd"><input style="width:99%" name="zip" type="text" id="city" value="<?php echo $zip ?>" size="8" /></td>
+                <td class="odd"><? echo $webui_admin_edit_manage_real_zip; ?></td>
+                <td class="odd"><input style="width:99%" name="zip" type="text" id="city" value="<?=$zip?>" size="8" /></td>
             </tr>
             
             <tr>                            
-                <td class="even"><?php echo $webui_admin_edit_manage_real_city; ?></td>
-                <td class="even"><input style="width:99%" name="city" type="text" id="street2" value="<?php echo $city ?>" /></td>                    
+                <td class="even"><? echo $webui_admin_edit_manage_real_city; ?></td>
+                <td class="even"><input style="width:99%" name="city" type="text" id="street2" value="<?=$city?>" /></td>                    
             </tr>
             
             <tr>
-                <td class="odd"><?php echo $webui_admin_edit_manage_real_country; ?></td>
+                <td class="odd"><? echo $webui_admin_edit_manage_real_country; ?></td>
                 <td class="odd">
                     <select style="width:100%" class="box" wide="25" name="country">
-                    <?php
+                    <?
                       $DbLink->query("SELECT name FROM ".C_COUNTRY_TBL." ORDER BY name ASC ");
 	                     while(list($COUNTRYDB) = $DbLink->next_record())
 	                     { ?>
-	                       <option <?php if($country == $COUNTRYDB){ echo"selected";} ?> value="<?php echo $COUNTRYDB ?>"><?php echo $COUNTRYDB ?></option>
-                         <?php } ?>
+	                       <option <? if($country == $COUNTRYDB){ echo"selected";} ?> value="<?=$COUNTRYDB?>"><?=$COUNTRYDB?></option>
+                         <? } ?>
                     </select>
                 </td>
             </tr>
       
       		  <tr>
-                <td class="even"><?php echo $webui_admin_edit_manage_real_email; ?></td>
-                <td class="even"><input style="width:99%" name="email" type="text" id="email" value="<?php echo $email ?>" /></td>
+                <td class="even"><? echo $webui_admin_edit_manage_real_email; ?></td>
+                <td class="even"><input style="width:99%" name="email" type="text" id="email" value="<?=$email?>" /></td>
             </tr>
 
             
             <tr>
                 <td colspan="2" class="odd">
                     <div align="center">
-                        <input type="submit" name="Submit2" value="<?php echo $webui_admin_edit_manage_savechanges; ?>" />
+                        <input type="submit" name="Submit2" value="<? echo $webui_admin_edit_manage_savechanges; ?>" />
                     </div>
                 </td>
             </tr>
@@ -155,23 +154,23 @@ else
             <form name="form1" method="post" action="index.php?page=adminedit">
 
             <tr>
-                <td class="even"><?php echo $webui_admin_edit_manage_currentstatus; ?></td>
+                <td class="even"><? echo $webui_admin_edit_manage_currentstatus; ?></td>
                 <td class="even">
-                  <?php if($active==1){echo"<font COLOR=#00FF00>$webui_admin_edit_manage_active</font>";}
+                  <? if($active==1){echo"<font COLOR=#00FF00>$webui_admin_edit_manage_active</font>";}
 				     else{echo"<font COLOR=#FF0000>$webui_admin_edit_manage_inactive</font>";}
                   ?>
                 </td>
             </tr>
               
             <tr>
-                <td class="odd"><?php echo $webui_admin_edit_manage_setstatus; ?></td>
+                <td class="odd"><? echo $webui_admin_edit_manage_setstatus; ?></td>
                 <td class="odd">
                     <input type="hidden" name="state" value="set" />
-                    <input type="hidden" name="userid" value="<?php echo $uuid ?>" />
+                    <input type="hidden" name="userid" value="<?=$uuid?>" />
                     
                     <select name="status">
-                        <option value="1"><?php echo $webui_admin_edit_manage_active; ?></option> 
-                        <option value="0"><?php echo $webui_admin_edit_manage_inactive; ?></option>
+                        <option value="1"><? echo $webui_admin_edit_manage_active; ?></option> 
+                        <option value="0"><? echo $webui_admin_edit_manage_inactive; ?></option>
                     </select>
                 </td>
             </tr>
@@ -179,14 +178,13 @@ else
             <tr>
                 <td colspan="2" class="odd">
                     <div align="center">
-                        <input type="submit" name="Submit" value="<?php echo $webui_admin_edit_manage_savestatus; ?>" />
+                        <input type="submit" name="Submit" value="<? echo $webui_admin_edit_manage_savestatus; ?>" />
                     </div>
                 </td>
             </tr>
             
             </form>
         </table>
-		</div>
     </div>
 </div>
-<?php } ?>
+<? } ?>
