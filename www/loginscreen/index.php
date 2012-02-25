1c558f9a8b611735b6486da743dfb529b80d8ce8
SignpostMarv <github@signpostmarv.name>
2012-02-25 18:01:30 +0000

    using new method on webui instead of an SQL query

--- www/loginscreen/index.php
+++ www/loginscreen/index.php
@@ -19,42 +19,10 @@ $DbLink->query("SELECT gridstatus,active,color,title,message  FROM ".C_INFOWINDO
 list($GRIDSTATUS,$INFOBOX,$BOXCOLOR,$BOX_TITLE,$BOX_INFOTEXT) = $DbLink->next_record();
 
 $GRIDSTATUS = Configs::d()->OnlineStatus()->Online();
-
-// Doing it the same as the Who's Online now part
-$DbLink = new DB;
-$DbLink->query("SELECT UserID FROM ".C_USERINFO_TBL." where IsOnline = 1 AND ".
-				"LastLogin < (UNIX_TIMESTAMP(FROM_UNIXTIME(UNIX_TIMESTAMP(now())))) AND ".
-				"LastLogout < (UNIX_TIMESTAMP(FROM_UNIXTIME(UNIX_TIMESTAMP(now())))) ".
-				"ORDER BY LastLogin DESC");
-$NOWONLINE = 0;
-
-while(list($UUID) = $DbLink->next_record())
-{
-// Let's get the user info
-$DbLink3 = new DB;
-$DbLink3->query("SELECT CurrentRegionID from ".C_USERINFO_TBL." where UserID = '".cleanQuery($UUID)."'");
-list($RegionUUID) = $DbLink3->next_record();
-
-$DbLink2 = new DB;
-$DbLink2->query("SELECT FirstName, LastName from ".C_USERS_TBL." where PrincipalID = '".cleanQuery($UUID)."'");
-list($firstname, $lastname) = $DbLink2->next_record();
-$username = $firstname." ".$lastname;
-// Let's get the region information
-$DbLink3 = new DB;
-$DbLink3->query("SELECT RegionName from ".C_REGIONS_TBL." where RegionUUID = '".cleanQuery($RegionUUID)."'");
-list($region) = $DbLink3->next_record();
-if ($region != "")
-{
-$NOWONLINE = $NOWONLINE + 1;
-}
-}
-
-$DbLink->query("SELECT count(*) FROM ".C_USERINFO_TBL." where LastLogin > UNIX_TIMESTAMP(FROM_UNIXTIME(UNIX_TIMESTAMP(now()) - 2419200))");
-list($LASTMONTHONLINE) = $DbLink->next_record();
-
-$USERCOUNT = Configs::d()->FindUsers()->count();
-$REGIONSCOUNT = Configs::d()->GetRegions()->count();
-		
+$NOWONLINE       = Configs::d()->RecentlyOnlineUsers(0,true);
+$LASTMONTHONLINE = Configs::d()->RecentlyOnlineUsers(2419200,false);
+$USERCOUNT       = Configs::d()->FindUsers()->count();
+$REGIONSCOUNT    = Configs::d()->GetRegions()->count();
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
