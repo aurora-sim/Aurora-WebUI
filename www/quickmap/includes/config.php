9f3fb92e093916038d3daea35f533a45d79a4573
SignpostMarv <github@signpostmarv.name>
2012-03-07 16:15:48 +0000

    using main config options

--- www/quickmap/includes/config.php
+++ www/quickmap/includes/config.php
@@ -1,1 +1,35 @@
-<?// Change the following Parameters as needed. // General items// For Statics Images Folderdefine("SYSURL","***"); //Your Grid-Domain$CONF_sim_domain =     "***";//Installation path$CONF_install_path =   "***";// style-sheet items//Link to StyleSheet$CONF_style_sheet =    "templates/default/style.css";$CONF_style_sheet_webui =    "templates/default/style_webui.css";// Your Grid-Logo// Link to your Grid-Logo$CONF_logo =           "***/quickmap/templates/default/images/aurora-webui-logo.png"; // mysql database items$CONF_db_server =      "***";   // Your Database-Server$CONF_db_user =        "***";        // Database-User$CONF_db_pass =        "***";      // Password of User$CONF_db_database =    "***";  // Name of Da6tabase// The Coordinates of the Grid-Center$CONF_center_coord_x   = "1000";      // the Center-X-Coordinate$CONF_center_coord_y   = "1000";      // the Center-Y-Koordinate// Other Options$displayRoundedCorner = true;$displayLogoEffect = true;?>
+<?php
+// Change the following Parameters as needed. 
+
+// General items
+// For Statics Images Folder
+define("SYSURL","***"); 
+
+//Your Grid-Domain
+$CONF_sim_domain =     "***";
+//Installation path
+$CONF_install_path =   "***";
+
+// style-sheet items
+//Link to StyleSheet
+$CONF_style_sheet =    "templates/default/style.css";
+$CONF_style_sheet_webui =    "templates/default/style_webui.css";
+
+// Your Grid-Logo
+// Link to your Grid-Logo
+$CONF_logo =           "***/quickmap/templates/default/images/aurora-webui-logo.png"; 
+
+// mysql database items
+$CONF_db_server =      C_DB_HOST;   // Your Database-Server
+$CONF_db_user =        C_DB_USER;        // Database-User
+$CONF_db_pass =        C_DB_PASS;      // Password of User
+$CONF_db_database =    C_DB_NAME;  // Name of Da6tabase
+
+// The Coordinates of the Grid-Center
+$CONF_center_coord_x   = "1000";      // the Center-X-Coordinate
+$CONF_center_coord_y   = "1000";      // the Center-Y-Koordinate
+
+// Other Options
+$displayRoundedCorner = true;
+$displayLogoEffect = true;
+?>
