dea81891bc5ff90c8e67c893a5d79b21badab875
SignpostMarv <github@signpostmarv.name>
2012-03-19 03:35:20 +0000

    switching to use lib, tidying up html

--- www/app/agent/index.php
+++ www/app/agent/index.php
@@ -1,4 +1,4 @@
-<?
+<?php
 include("../../settings/config.php");
 include("../../settings/databaseinfo.php");
 include("../../settings/json.php");
@@ -7,24 +7,42 @@ include("../../languages/translator.php");
 include("../../templates/templates.php");
 use Aurora\Addon\WebUI\Configs;
 
-if ($_GET[name]) {
-    $userName = $_GET['name'];
+if (isset($_GET['name'])){
+	try{
+		$profile = Configs::d()->GetProfile($_GET['name']);
+	}catch(Aurora\Addon\UnexpectedValueException $e){
+		require_once('../../sites/404.php');
+		return;
+	}
 
-    $found = array();
-    $found[0] = json_encode(array('Method' => 'GetProfile', 'WebPassword' => md5(WEBUI_PASSWORD),
-        'Name' => cleanQuery($_GET['name'])));
-
-    $do_post_requested = do_post_request($found);
-    $recieved = json_decode($do_post_requested);
-
-    $profileTXT = $recieved->{'profile'}->{'AboutText'};
-    $profileImage = $recieved->{'profile'}->{'Image'};
-    $created = $recieved->{'account'}->{'Created'};
-    $UUID = $recieved->{'account'}->{'PrincipalID'};
-    $diff = $recieved->{'account'}->{'TimeSinceCreated'};
-    $type = $recieved->{'account'}->{'AccountInfo'};
-    $partner = $recieved->{'account'}->{'Partner'};
+	$userName = $profile->Name();
+    $profileTXT = $profile->AboutText();
+    $profileImage = $profile->Image();
+    $created = $profile->Created();
+    $UUID = $profile->PrincipalID();
+    $type = $profile->CustomType();
+    $partner = $profile->PartnerUUID() !== '00000000-0000-0000-0000-000000000000' ? Configs::d()->GetProfile('', $profile->PartnerUUID())->Name() : '';
     $date = date("D d M Y - g:i A", $created);
+	$profileLink = ($profileImage == '00000000-0000-0000-0000-000000000000') ? 'info.jpg' : Configs::d()->GridTexture($profileImage);
+
+    $difftime = $_SERVER['REQUEST_TIME'] - $profile->Created();
+	$diff = array();
+	if($difftime >= 31449600){
+		$diff['year'] = ($difftime - ($difftime % 31449600)) / 31449600;
+		$difftime %= 31449600;
+	}
+	if($difftime >= 604800){
+		$diff['week'] = ($difftime - ($difftime % 604800)) / 604800;
+		$difftime %= 604800;
+	}
+	if($difftime >= 86400){
+		$diff['day'] = (int)ceil(($difftime - ($difftime % 86400)) / 86400);
+	}
+	$difftime = array();
+	foreach($diff as $k=>$v){
+		$difftime[] = $v . ' ' . $k . ($v > 1 ? 's' : '');
+	}
+	$diff = implode(', ', $difftime);
 } 
 
 
@@ -56,81 +74,51 @@ $displayRss                      = $adminmodules['displayRss'];
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" href="<?= SYSURL ?><? echo $template_css ?>" type="text/css" />
-    <link rel="icon" href="<?= SYSURL ?><?=$favicon_image?>" />
-    <title><?= SYSNAME ?>: <? echo $webui_users_profile; ?> <? echo $userName ?></title>
+    <link rel="stylesheet" href="../../<?php echo $template_css ?>" type="text/css" />
+    <link rel="icon" href="../../<?php echo $favicon_image; ?>" />
+    <title><?php echo SYSNAME, ': ', $webui_users_profile, ' ', $userName ?></title>
     
 <?php if($displayRoundedCorner)  { ?>
-<script src="<?= SYSURL ?>javascripts/jquery/jquery.min.js" type="text/javascript"></script>
-<script type="text/javascript" src="<?= SYSURL ?>javascripts/jquery/jquery.corner.js?v2.11"></script>
+<script src="../../javascripts/jquery/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="../../javascripts/jquery/jquery.corner.js?v2.11"></script>
 <script type="text/javascript">
-	  $("#profil_picture").corner("15px");
-		$('#container_popup, #content_popup').corner();
+$("#profil_picture").corner("15px");
+$('#container_popup, #content_popup').corner();
 </script>
 <?php } ?>
-
 </head>
-
 <body class="webui">
 <div id="container_popup">
-<div id="content_popup">
-  <h2><?= SYSNAME ?>: <? echo $webui_users_profile; ?> <? echo $userName ?></h2>
-  
-  <div id="useragentprofil">
-	<!--  <div id="info"><p><? echo $webui_region_list_page_info ?></p></div> -->
-
-  <hr>
-      <table>
-          <tr>
-              <td>
-                  <? echo $webui_resident_since ?>: <?= $date ?> <br /> <? echo $webui_resident_age ?>: (<?= $diff ?>)
-              </td>
-          </tr>
-          <tr>
-              <td>
-                  <? echo $webui_account_info ?>: <?= $type ?>
-              </td>
-          </tr>         
-          <tr>
-              <td></td>
-          </tr>
-              <? if ($partner != '') { ?>
-          <tr>
-              <td>
-                  <? echo $webui_partner; ?>: <? echo $partner ?>
-              </td>
-          </tr>
-          <tr>
-              <td></td>
-          </tr>
-              <? } ?>
-          <tr>
-              <td>
-                  <h2><? echo $webui_about_me; ?></h2>
-              </td>
-          </tr>       
-          <tr>
-              <td>
-                <?
-                  if ($profileTXT != '') { echo $profileTXT; }
-                  else { echo $webui_no_information_set; }
-                ?>
-              </td>
-          </tr>
-      </table>
-      
-      <div id="profil_picture">
-          <?
-            if($profileImage == "")
-            {
-                $profileLink = "info.jpg";
-            }
-            else
-                $profileLink = WEBUI_TEXTURE_SERVICE . '/index.php?method=GridTexture&uuid=' . $profileImage;
-          ?>
-          <img alt="<? echo $profileImage ?>" src="<? echo $profileLink ?>" title="<? echo $userName ?>" />
-      </div>
-  </div>
-</div>  </div>
+	<div id="content_popup">
+		<h2><?php echo SYSNAME, ': ', $webui_users_profile, ' ', $userName ?></h2>
+		<div id="useragentprofil">
+			<hr/>
+			<table>
+				<tr>
+					<td><?php echo $webui_resident_since ?>: <?php echo $date ?> <br /> <?php echo $webui_resident_age ?>: (<?php echo $diff ?>)</td>
+				</tr>
+				<tr>
+					<td><?php echo $webui_account_info ?>: <?php echo $type ?></td>
+				</tr>
+<?php if ($partner != '') { ?>
+				<tr>
+					<td>
+					<?php echo $webui_partner; ?>: <?php echo $partner ?>
+					</td>
+				</tr>
+<?php } ?>
+				<tr>
+					<td><h2><?php echo $webui_about_me; ?></h2></td>
+				</tr>
+				<tr>
+					<td><?php echo trim($profileTXT) !== '' ? trim($profileTXT) : $webui_no_information_set; ?></td>
+				</tr>
+			</table>
+			<div id="profil_picture">
+				<img alt="<?php echo $profileImage ?>" src="<?php echo $profileLink ?>" title="<?php echo $userName ?>" />
+			</div>
+		</div>
+	</div>
+</div>
 </body>
 </html>
