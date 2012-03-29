f54be0fccf4429373aaf8639b8c763e914f296ec
SignpostMarv <github@signpostmarv.name>
2012-03-29 23:34:16 +0100

    cleaning up code, switching to libAurora.php

--- www/sites/main/activatemail.php
+++ www/sites/main/activatemail.php
@@ -6,17 +6,8 @@ if(isset($_GET['code'])){
 	$DbLink = new DB;
 	$DbLink->query("SELECT UUID, email FROM ".C_CODES_TBL." WHERE code='".cleanQuery($_GET['code'])."' and info='emailconfirm'");
 	list($UUID, $EMAIL) = $DbLink->next_record();
-	if($UUID){
-		$found = array();
-		$found[0] = json_encode(array(
-			'Method' => 'SaveEmail',
-			'WebPassword' => md5(WEBUI_PASSWORD),
-			'UUID' => cleanQuery($UUID),
-			'Email' => cleanQuery($EMAIL)
-		));
-		$do_post_requested = do_post_request($found);
-		$recieved = json_decode($do_post_requested);
-		if ($recieved->{'Verified'} == 1){
+	if(isset($UUID, $EMAIL)){
+		if (Configs::d()->SaveEmail( $UUID, $EMAIL)){
 			$WERROR="Thank you, your email address was changed";
 			$DbLink->query("DELETE FROM ".C_CODES_TBL." WHERE code='".cleanQuery($_GET[code])."' and info='emailconfirm'");
 		}
