b74b9bd51e63e030275085af287e51942924e35b
SignpostMarv <github@signpostmarv.name>
2012-03-29 23:33:10 +0100

    cleaning up code

--- www/sites/main/activatemail.php
+++ www/sites/main/activatemail.php
@@ -1,50 +1,40 @@
-<?
-if($_GET[code]){
-$DbLink = new DB;
+<?php
+$UUID   = null;
+$WERROR = "This isnt a valid code or maybe the code was older than 24h";
 
-$DbLink->query("SELECT UUID, email FROM ".C_CODES_TBL." WHERE code='".cleanQuery($_GET[code])."' and info='emailconfirm'");
-list($UUID, $EMAIL) = $DbLink->next_record();
-}
-
-if($UUID)
-{
-	$found = array();
-	$found[0] = json_encode(array('Method' => 'SaveEmail', 'WebPassword' => md5(WEBUI_PASSWORD)
-		, 'UUID' => cleanQuery($UUID)
-		, 'Email' => cleanQuery($EMAIL)));
-	$do_post_requested = do_post_request($found);
-	$recieved = json_decode($do_post_requested);
-
-	if ($recieved->{'Verified'} == 1)
-	{
-		$WERROR="Thank you, your email address was changed";
-		$DbLink->query("DELETE FROM ".C_CODES_TBL." WHERE code='".cleanQuery($_GET[code])."' and info='emailconfirm'");
+if(isset($_GET['code'])){
+	$DbLink = new DB;
+	$DbLink->query("SELECT UUID, email FROM ".C_CODES_TBL." WHERE code='".cleanQuery($_GET['code'])."' and info='emailconfirm'");
+	list($UUID, $EMAIL) = $DbLink->next_record();
+	if($UUID){
+		$found = array();
+		$found[0] = json_encode(array(
+			'Method' => 'SaveEmail',
+			'WebPassword' => md5(WEBUI_PASSWORD),
+			'UUID' => cleanQuery($UUID),
+			'Email' => cleanQuery($EMAIL)
+		));
+		$do_post_requested = do_post_request($found);
+		$recieved = json_decode($do_post_requested);
+		if ($recieved->{'Verified'} == 1){
+			$WERROR="Thank you, your email address was changed";
+			$DbLink->query("DELETE FROM ".C_CODES_TBL." WHERE code='".cleanQuery($_GET[code])."' and info='emailconfirm'");
+		}
 	}
-}
-else
-{
-	$WERROR="This isnt a valid code or maybe the code was older than 24h";
 }
 ?>
 
-<style type="text/css">
-<!--
-.Stil1 {
-	font-size: 18px;
-	font-weight: bold;
-}
--->
-</style>
 <table width="100%" border="0" align="center">
 	<tr>
-		<td valign="top"><table width="50%" border="0" align="center">
-			<tr>
-				<td><p align="center" class="Stil1">Activate New Email</p></td>
-			</tr>
-                        </table>
+		<td valign="top">
+			<table width="50%" border="0" align="center">
+				<tr>
+					<td><p align="center" class="Stil1" style="font-size:18px;font-weight:bold;">Activate New Email</p></td>
+				</tr>
+			</table>
 		</td>
 	</tr>
 	<tr>
-		<td valign="top"><?=$WERROR?></td>
+		<td valign="top"><?php echo $WERROR; ?></td>
 	</tr>
 </table>
