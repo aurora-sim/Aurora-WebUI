36eb8d6e8b0bb79749788a52beb2faaf108a5557
SignpostMarv <github@signpostmarv.name>
2012-03-29 23:28:51 +0100

    cleaning up code

--- www/sites/main/activate.php
+++ www/sites/main/activate.php
@@ -1,52 +1,49 @@
-<?
-
-if($_GET[code])
-{
+<?php
+$WERROR = $UUID = null;
+if(isset($_GET['code'])){
 	$DbLink = new DB;
-
 	$DbLink->query("SELECT UUID FROM ".C_CODES_TBL." WHERE code='".cleanQuery($_GET[code])."' and info='confirm'");
 	list($UUID) = $DbLink->next_record();
 }
 
-if($UUID)
-{
+if(isset($UUID)){
 	$found = array();
 	$found[0] = json_encode(array('Method' => 'Authenticated', 'WebPassword' => md5(WEBUI_PASSWORD), 'UUID' => cleanQuery($UUID)));
 	$do_post_requested = do_post_request($found);
 	$recieved = json_decode($do_post_requested);
-	if ($recieved->{'Verified'} == "true")
-	{
+	if($recieved->{'Verified'} == "true"){
 		$WERROR= $webui_verified_account;
 		$DbLink->query("DELETE FROM ".C_CODES_TBL." WHERE code='".cleanQuery($_GET[code])."' and info='confirm'");
+	}else{
+		$WERROR = $webui_internal_error;
 	}
-	else
-	{
-		$WERROR=$webui_internal_error;
-	}
-}
-else
-{
-	$WERROR=$webui_invalid_code;
+}else{
+	$WERROR = $webui_invalid_code;
 }
 ?>
 
 <table width="100%" height="425" border="0" align="center">
-            <tr>
-              <td valign="top"><table width="50%" border="0" align="center">
-                <tr>
-                  <td><p align="center" class="Stil1"><? echo $webui_activate_account ?></p></td>
-                </tr>
-              </table>
-              <br />
-              <table width="79%" height="199" border="0" align="center" cellpadding="5" cellspacing="5" bgcolor="#FFFFFF">
-                <tr>
-                <td valign="top"><br>
-                  <br>
-                  <? if($WERROR){?>
-				  <font color="FF0000"><?=$WERROR?></font><br>
-				  <? } ?>
-                  <br>
-                </tr>
-              </table></td>
-            </tr>
-        </table>
+	<tr>
+		<td valign="top">
+			<table width="50%" border="0" align="center">
+				<tr>
+					<td><p align="center" class="Stil1"><? echo $webui_activate_account ?></p></td>
+				</tr>
+			</table>
+			<br />
+			<table width="79%" height="199" border="0" align="center" cellpadding="5" cellspacing="5" bgcolor="#FFFFFF">
+				<tr>
+					<td valign="top">
+						<br />
+						<br />
+<?php if(isset($WERROR)){ ?>
+						<font color="FF0000"><?=$WERROR?></font>
+						<br />
+<?php } ?>
+						<br />
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>
