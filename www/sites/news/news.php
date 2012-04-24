3e65e8e04946df1993d14b9dd32f735036bc3988
SignpostMarv <github@signpostmarv.name>
2012-04-24 22:58:31 +0100

    detecting when invalid page is specified and redirecting to previous page. fixing math bug that caused next page link to appear early

--- www/sites/news/news.php
+++ www/sites/news/news.php
@@ -13,8 +13,13 @@
 	use Aurora\Addon\WebUI\Configs;
 
 	$querypage = max(isset($_GET['pagenum']) ? (integer)$_GET['pagenum'] : 0, 0);
-	$news = isset($_GET['scr']) ? null : Configs::d()->NewsFromGroupNotices($querypage * 5, 5);
-	$showNext = isset($news) ? (($querypage * 5) + 5) <= $news->count() : false;
+	try{
+		$news = isset($_GET['scr']) ? null : Configs::d()->NewsFromGroupNotices($querypage * 5, 5);
+	}catch(Aurora\Addon\LengthException $e){
+		header('Location: ' . SYSURL . 'index.php?page=news&pagenum=' . ($querypage - 1));
+		exit;
+	}
+	$showNext = isset($news) ? (($querypage * 5) + 5) < $news->count() : false;
 ?>
 <!-- STYLE TO DO -->
         <div style="text-align: left; width: 50%; float: left;">
