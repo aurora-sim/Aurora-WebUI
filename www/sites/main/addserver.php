31c03bd8b1dd3dc8691aeedab41b75ac41fecbea
SignpostMarv <github@signpostmarv.name>
2012-04-25 16:55:49 +0100

    cleaning up code

--- www/sites/main/addserver.php
+++ www/sites/main/addserver.php
@@ -1,14 +1,10 @@
 <div id="content">
-  <div id="ContentHeaderLeft"><h5><?= SYSNAME ?></h5></div>
-  <div id="ContentHeaderCenter"></div>
-  <div id="ContentHeaderRight"><h5><? echo $webui_addserver; ?></h5></div>
-  <div id="gridinfos">
-  <div id="info"><p><? echo $webui_addserver_info; ?></p></div>
-  <div class="clear"></div>
-
-	<p>
-		<div id="annonce7"><? echo $webui_addserver; ?> ...<br>...<br>...</div>
-	</p>
-
+	<div id="ContentHeaderLeft"><h5><?php echo SYSNAME; ?></h5></div>
+	<div id="ContentHeaderCenter"></div>
+	<div id="ContentHeaderRight"><h5><?php echo $webui_addserver; ?></h5></div>
+	<div id="gridinfos">
+		<div id="info"><p><?php echo $webui_addserver_info; ?></p></div>
+		<div class="clear"></div>
+		<p><div id="annonce7"><?php echo $webui_addserver; ?> ...<br>...<br>...</div></p>
 	</div>
 </div>
--- www/sites/main/chat.php
+++ www/sites/main/chat.php
@@ -1,23 +1,18 @@
 <div id="content">
-  <div id="ContentHeaderLeft"><h5><?= SYSNAME ?></h5></div>
-  <div id="ContentHeaderCenter"></div>
-  <div id="ContentHeaderRight"><h5><? echo $webui_chat; ?></h5></div>
-
-  <div id="info">
-    <p><? echo $webui_chat_info; ?> Basic IRC chat help <a href="http://www.ircbeginner.com/ircinfo/ircc-commands.html" target="_blank">here</a>.</p>
-  </div>
-
-  <div class="clear"></div>
-    <div id="chat">
-      <iframe class="level1"
-              src="http://webchat.freenode.net/?channels=<?=$support_IRC_channel?>&uio=MTE9MjI2dd"
-              width="100%"
-              height="400"
-              border="0"
-              frameborder="0"
-              framespacing="0"
-              allowTransparency="true"
-              style="z-index:0">
-      </iframe>
-    </div>
+	<div id="ContentHeaderLeft"><h5><?php echo SYSNAME; ?></h5></div>
+	<div id="ContentHeaderCenter"></div>
+	<div id="ContentHeaderRight"><h5><?php echo $webui_chat; ?></h5></div>
+	<div id="info"><p><?php echo $webui_chat_info; ?> Basic IRC chat help <a href="http://www.ircbeginner.com/ircinfo/ircc-commands.html" target="_blank">here</a>.</p></div>
+	<div class="clear"></div>
+	<div id="chat">
+		<iframe class="level1"
+			src="http://webchat.freenode.net/?channels=<?php echo $support_IRC_channel; ?>&amp;uio=MTE9MjI2dd"
+			width="100%"
+			height="400"
+			border="0"
+			frameborder="0"
+			framespacing="0"
+			allowTransparency="true"
+			style="z-index:0"></iframe>
+	</div>
 </div>
