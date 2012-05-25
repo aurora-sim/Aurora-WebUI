1e5f687c48973e8c918f500425fff65371dddb36
SignpostMarv <github@signpostmarv.name>
2012-05-25 18:16:05 +0100

    minor tweaks to layout of code

--- www/sites/main/home.php
+++ www/sites/main/home.php
@@ -1,26 +1,23 @@
-<div id="content">
+					<div id="content">
 <?php if($displayDate) { ?>
-<div id="ContentHeaderLeft"><h5><?= SYSNAME ?></h5></div>
+						<div id="ContentHeaderLeft"><h5><?php echo SYSNAME; ?></h5></div>
+						<div id="ContentHeaderCenter">
+							<div id="date"><?php
+		$date = date("d-m-Y");
+		$heure = date("H:i");
+		echo $webui_before_date, ' ', $date, ' ', $heure;
+?>
+							</div>
+						</div>
+						<div id="ContentHeaderRight"><h5><?php echo $webui_home; ?></h5></div>
 
-<div id="ContentHeaderCenter">
-    <div id="date">
-     <?php $date = date("d-m-Y");
-    $heure = date("H:i");
-    Print("$webui_before_date $date $webui_after_date $heure");
-    ?>
-    </div>
-</div>
-
-<div id="ContentHeaderRight"><h5><? echo $webui_home; ?></h5></div>
 <?php } ?>
-
-<div class="clear"></div>
-        
-<? include("sites/modules/steps123.php"); ?>
-        
-<div id="annonce7"><h3><? echo $webui_welcome; ?> <?= SYSNAME ?></h3><? echo $webui_home_page; ?></div>
-<div id="annonce10"><p><? echo $webui_home_page_warning; ?></p></div>
-
-<? include("sites/modules/infos123.php"); ?>
-
-</div>
+						<div class="clear"></div>
+<?php include("sites/modules/steps123.php"); ?>
+						<div id="annonce7">
+							<h3><?php echo $webui_welcome, ' ', SYSNAME; ?></h3>
+							<?php echo str_replace(array('        ', "\t\t"), "\t\t\t\t\t\t\t", trim($webui_home_page)), "\n"; ?>
+						</div>
+						<div id="annonce10"><p><? echo $webui_home_page_warning; ?></p></div>
+<?php include("sites/modules/infos123.php"); ?>
+					</div>
--- www/sites/modules/infos123.php
+++ www/sites/modules/infos123.php
@@ -1,28 +1,14 @@
-<div id="info1"><h3><? echo $webui_infos_comment01_title; ?></h3> 
-  <p><? echo $webui_infos_comment01; ?></p>
-</div>
-  
-<div id="info2"><h3><? echo $webui_infos_comment02_title; ?></h3>
-  <p><? echo $webui_infos_comment02; ?></p>
-</div>
+						<div id="info1">
+							<h3><?php echo $webui_infos_comment01_title; ?></h3>
+							<p><?php echo $webui_infos_comment01; ?></p>
+						</div>
 
-<div id="info3"><h3><? echo $webui_infos_comment03_title; ?></h3>
-  <p><? echo $webui_infos_comment03; ?></p>
-</div>
+						<div id="info2">
+							<h3><?php echo $webui_infos_comment02_title; ?></h3>
+							<p><?php echo $webui_infos_comment02; ?></p>
+						</div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+						<div id="info3">
+							<h3><?php echo $webui_infos_comment03_title; ?></h3>
+							<p><?php echo $webui_infos_comment03; ?></p>
+						</div>
