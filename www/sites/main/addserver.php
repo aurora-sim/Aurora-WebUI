3a4598d0de975e7611f8c3a733f8b65e7d53eaa8
SignpostMarv <github@signpostmarv.name>
2012-05-25 21:41:16 +0100

    minor tweaks to layout of code

--- www/sites/main/addserver.php
+++ www/sites/main/addserver.php
@@ -1,10 +1,10 @@
-<div id="content">
-	<div id="ContentHeaderLeft"><h5><?php echo SYSNAME; ?></h5></div>
-	<div id="ContentHeaderCenter"></div>
-	<div id="ContentHeaderRight"><h5><?php echo $webui_addserver; ?></h5></div>
-	<div id="gridinfos">
-		<div id="info" style="float:left;"><p><?php echo $webui_addserver_info; ?></p></div>
-		<div class="clear"></div>
-		<p><div id="annonce7"><?php echo $webui_addserver; ?> ...<br>...<br>...</div></p>
-	</div>
-</div>
+					<div id="content">
+						<div id="ContentHeaderLeft"><h5><?php echo SYSNAME; ?></h5></div>
+						<div id="ContentHeaderCenter"></div>
+						<div id="ContentHeaderRight"><h5><?php echo $webui_addserver; ?></h5></div>
+						<div id="gridinfos">
+							<div id="info" style="float:left;"><p><?php echo $webui_addserver_info; ?></p></div>
+							<div class="clear"></div>
+							<p><div id="annonce7"><?php echo $webui_addserver; ?> ...<br>...<br>...</div></p>
+						</div>
+					</div>
--- www/sites/main/downloads.php
+++ www/sites/main/downloads.php
@@ -1,24 +1,24 @@
-<div id="content">
-	<div id="ContentHeaderLeft"><h5><?= SYSNAME ?></h5></div>
-		<div id="ContentHeaderCenter"></div>
-		<div id="ContentHeaderRight"><h5><? echo $webui_downloads; ?></h5></div>
-		<div id="gridinfos">
-			<div id="info" style="float:left;"><p><? echo $webui_downloads_info; ?></p></div>
-			<div class="clear"></div>
+					<div id="content">
+						<div id="ContentHeaderLeft"><h5><?= SYSNAME ?></h5></div>
+						<div id="ContentHeaderCenter"></div>
+						<div id="ContentHeaderRight"><h5><? echo $webui_downloads; ?></h5></div>
+						<div id="gridinfos">
+							<div id="info" style="float:left;"><p><? echo $webui_downloads_info; ?></p></div>
+							<div class="clear"></div>
 
-			<div id="download1">
-				<h3><? echo $webui_downloads_imprudence_windows_title; ?></h3>
-				<p><? echo $webui_downloads_imprudence_windows; ?></p>
-			</div>
-  
-			<div id="download2">
-				<h3><? echo $webui_downloads_imprudence_linux_title; ?></h3>
-				<p><? echo $webui_downloads_imprudence_linux; ?></p>
-			</div>
-  
-			<div id="download3">
-				<h3><? echo $webui_downloads_imprudence_mac_title; ?></h3>
-				<p><? echo $webui_downloads_imprudence_mac; ?></p>
-			</div>
-		</div>
-</div>
+							<div id="download1">
+								<h3><? echo $webui_downloads_imprudence_windows_title; ?></h3>
+								<p><? echo $webui_downloads_imprudence_windows; ?></p>
+							</div>
+				  
+							<div id="download2">
+								<h3><? echo $webui_downloads_imprudence_linux_title; ?></h3>
+								<p><? echo $webui_downloads_imprudence_linux; ?></p>
+							</div>
+				  
+							<div id="download3">
+								<h3><? echo $webui_downloads_imprudence_mac_title; ?></h3>
+								<p><? echo $webui_downloads_imprudence_mac; ?></p>
+							</div>
+						</div>
+					</div>
