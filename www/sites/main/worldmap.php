11e8d937923d22d0dd145cac9d65c3598df11bd4
djphil <digitalconcepts@free.fr>
2013-04-22 13:45:33 +0300

    Update worldmap.php

--- www/sites/main/worldmap.php
+++ www/sites/main/worldmap.php
@@ -1,21 +1,22 @@
-<div id="content">
-  <div id="ContentHeaderLeft"><h5><p><?= SYSNAME ?></p></h5></div>
-  <div id="ContentHeaderCenter"></div>
-  <div id="ContentHeaderRight"><h5><p><? echo $webui_world_map; ?></p></h5></div>
-    <style type="text/css">
-		#map-container {width: 100%; height: 445px;}
-    </style>
-  <div id="map-container">
-  </div>
-  <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key="></script>
-  <script src="map/slmapapi.php"></script>
-  <link rel="stylesheet" type="text/css" href="map/slmapapi.css" />
+<script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key="></script>
+<script src="map/slmapapi.php"></script>
+<link rel="stylesheet" type="text/css" href="map/slmapapi.css" />
+<style type="text/css">#map-container {width: 100%; height: 380px;}</style>
 
-  <script type="text/javascript">
+<script type="text/javascript">
     function loadmap(){
-        var coords = {'x' : <?  echo $mapstartX ?> + 0.5, 'y' : <? echo $mapstartY ?> + 0.5},
+        var coords = {'x' : <?php echo $mapstartX; ?> + 0.5, 'y' : <?php echo $mapstartY; ?> + 0.5},
         mapInstance = new SLURL.Map(document.getElementById('map-container'), {'overviewMapControl':true});
         mapInstance.centerAndZoomAtSLCoord(new SLURL.XYPoint(coords.x, coords.y), 3);}
     $(document).ready(loadmap);
-  </script>
+</script>
+
+<div id="content">
+    <div id="ContentHeaderLeft"><h5><?php echo SYSNAME; ?>: <?php echo $webui_world_map; ?></h5></div>
+    <div id="ContentHeaderCenter"></div>
+    <div id="ContentHeaderRight">
+        <h5><a onClick="window.open('<?php echo SYSURL; ?>map/index.php','mywindow')" style="cursor:pointer; float:right; display:inline-block;"><?php echo $webui_fullscreen; ?></a></h5>
+    </div>
+    <div class="clear"></div>
+    <div id = "map-container"></div>
 </div>
